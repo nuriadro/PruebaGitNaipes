383a8c29555c70172085086beba0d06497f46894
nuria <nrioolm101@g.educaand.es>
2023-12-01 09:28:24 +0100

    Inicio del programa

---
+++ src/main/java/com/mycompany/pruebagithub/Naipe.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebagithub;
+
+import java.util.Random;
+
+/**
+ *
+ * @author nuria
+ */
+public class Naipe {
+   
+        //clase naipe que tendrá cartas (1-7, 8 (sota), 9(caballo) 10(rey))
+        //palo  (oros, bastos, espada, copas)
+       
+        //constructor aleatorio por defecto y parámetros () lanza excepción en caso de parámetros erróneos
+        
+        //creamos una clase baraja, que, como atributo va a tener un array de naipe que como constructor por defecto relleno el aray con todos los naipes
+    
+    private int carta;
+    private String naipe;
+    
+//    public Naipe(){
+//        
+//        //generamos una carta random 
+//        Random random = new Random();
+//        this.carta = 
+//    
+//}
+                                                                    
+        
+        
+    
+}
